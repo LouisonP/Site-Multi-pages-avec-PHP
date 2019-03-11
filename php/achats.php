d67a8430e9dbc6f0eb56c189e5d8447117f95722
LouisonP <Louison.Poiriez.pro@gmail.com>
2019-03-11 16:24:09 +0100

    Add page achats

---
+++ php/achats.php
@@ -0,0 +1,11 @@
+<?php
+include_once('php/header.php');//PHP on l'inclu à l'endroit ou il doit se mett
+?>
+<!-- include nav en exemple -->
+<!-- Pour mettre en commentaire PLUS RAPIDEMENT on sélectionne le texte que 
+l'on veut mettre en commentaire et on clique sur ctrl et la touche slash -->
+<!--On écrit ici entre le header et le footer ici dans notre page achat pour créer la page achat.
+<?php
+include_once('php/footer.php');
+?>
+
