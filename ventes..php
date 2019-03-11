494c115ad3d8914bcb67fa71ad1dd290bfe44da1
alexandre <alexandre.douchet.pro@gmail.com>
2019-03-11 16:38:47 +0100

    add âge vente

---
+++ ventes..php
@@ -0,0 +1,10 @@
+<?php
+include_once('php/header.php');//PHP on l'inclu à l'endroit ou il doit se mett
+?>
+<!-- include nav en exemple -->
+<!-- Pour mettre en commentaire PLUS RAPIDEMENT on sélectionne le texte que 
+l'on veut mettre en commentaire et on clique sur ctrl et la touche slash -->
+
+<?php
+include_once('php/footer.php');
+?>
