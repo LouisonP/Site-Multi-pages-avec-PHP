14eb46cab6f9041960069539e6dbf2c65a13a080
parshimipopeli <philippe.lamorskipro@gmail.com>
2019-03-12 12:28:19 +0100

    Update

--- php/footer.php
+++ php/footer.php
@@ -4,6 +4,7 @@
 </footer>
 
 <link rel="stylesheet" href="css/app.min.css">
+<script src="js/app.js"></script>
 
 </body>
 </html>
--- php/header.php
+++ php/header.php
@@ -6,6 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Site multi-pages en PHP</title>
 </head>
+    <header>
+        <div class="baniere_nav">
+            <div class="toggle_btn">
+                <span></span>
+        </div>
+        <div class="menu nav">
+            <nav>
+                <ul>
+                    <li><a href="#acceuil">Acceuil</a></li>
+                    <li><a href="#ventes">Vendez votre bien</a></li>
+                    <li><a href="#achats">Achetez la maison de vos rêves</a></li>
+                    <li><a href="#contact">contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        </div>
+    
+    
+    
+    
+    </header>
 <body>
 
 <!--<nav></nav> //ex ou je peux mettre ma nav-->
