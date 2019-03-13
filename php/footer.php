c9a60854967032bc2bd03ddf652d885fedb376f2
parshimipopeli <philippe.lamorskipro@gmail.com>
2019-03-13 16:26:58 +0100

    h

--- php/footer.php
+++ php/footer.php
@@ -1,8 +1,7 @@
 <footer>
 </footer>
-
-<link rel="stylesheet" href="css/app.min.css">
 <link rel="stylesheet" href="/css/foundation.min.css">
+<link rel="stylesheet" href="css/app.min.css">
 <script src="/js/app.js"></script>
 
 </body>
